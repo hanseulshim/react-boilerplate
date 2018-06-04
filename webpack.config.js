433381159447d9c826db4e803e06aa735f9ae861
Hanseul Shim <hanseulshim@gmail.com>
2018-06-04 11:33:17 -0400

    add various plugins and sass support
    babel-loader, clean-webpack-plugin, css-loader, html-webpack-plugin, node-sass, sass-loader, style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -14,7 +16,27 @@ module.exports = {
         test: /\.js$/,
         exclude: ['node_modules'],
         use: [{ loader: 'babel-loader' }]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
       }
+    ],
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: 'index.html'
+      }),
+      new CleanWebpackPlugin(['dist'])
     ]
   }
 };
